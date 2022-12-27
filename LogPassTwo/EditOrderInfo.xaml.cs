000f48234a2b9b5fa059e1506e74e06a3896307d
Zelaos659 <nikita23345@gmail.com>
2022-12-27 12:08:03 +0300

    ПОСЛЕДНЮЮ КНОПКУ ПОЧТИ ДОДЕЛАЛ НУ

---
+++ LogPassTwo/EditOrderInfo.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static LogPassTwo.CustomMSGbox;
+
+namespace LogPassTwo
+{
+    /// <summary>
+    /// Логика взаимодействия для EditOrderInfo.xaml
+    /// </summary>
+    public partial class EditOrderInfo : Window
+    {
+        static EditOrderInfo newWin;
+        static string result;
+        public EditOrderInfo()
+        {
+            InitializeComponent();
+        }
+
+        public static string Show(int x)
+        {
+            newWin = new EditOrderInfo();
+            if (x == 1)
+            {
+                newWin.GroupTrack.Visibility = Visibility.Visible;
+            }
+            else if (x == 2)
+            {
+                newWin.GroupStatus.Visibility = Visibility.Visible;
+            }
+            newWin.ShowDialog();
+            return result;
+        }
+
+        private void btnOne(object sender, RoutedEventArgs e)
+        {
+            result = "Новый";
+            Close();
+        }
+
+        private void btnTwo(object sender, RoutedEventArgs e)
+        {
+            result = "Ждёт отправления";
+            Close();
+        }
+
+        private void btnThree(object sender, RoutedEventArgs e)
+        {
+            result = "Отправлен";
+            Close();
+        }
+
+        private void btnFour(object sender, RoutedEventArgs e)
+        {
+            result = "Получите на почте";
+            Close();
+        }
+
+        private void editNum(object sender, RoutedEventArgs e)
+        {
+            result = txtTrack.Text;
+            Close();
+        }
+    }
+}
---
+++ LogPassTwo/Pages/EditingOrders.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LogPassTwo.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для EditingOrders.xaml
+    /// </summary>
+    public partial class EditingOrders : Page
+    {
+        public EditingOrders()
+        {
+            InitializeComponent();
+        }
+
+        private void TNumEdit(object sender, RoutedEventArgs e)
+        {
+            txtTest.Text = EditOrderInfo.Show(1);
+        }
+
+        private void EditStat(object sender, RoutedEventArgs e)
+        {
+            txtTest.Text = EditOrderInfo.Show(2);
+        }
+    }
+}
